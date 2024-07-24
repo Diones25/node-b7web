e188caa40ae828eb5557154b579f3dde1a0ee471
Diones Pereira Alves <pereiradiones987@gmail.com>
2024-07-24 16:58:38 -0300

    Configuração básica do Express

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import 'dotenv/config';
 import express, { Request, Response } from 'express';
+import helmet from 'helmet';
+import path from 'path';
 
 const server = express();
 
+server.use(helmet());
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static(path.join(__dirname, '../public')));
+
 server.get('/', (req: Request, res: Response) => {
-  res.send('Hello world!');
+  res.json('Hello world!');
 })
 
 server.listen(process.env.PORT, () => {
