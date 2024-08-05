c3b547188f1e741f99c62e2dbe372e9e78ea307c
Diones Pereira Alves <pereiradiones987@gmail.com>
2024-08-05 11:22:52 -0300

    Entendendo o básico do zod

--- src/zod.ts
+++ src/zod.ts
@@ -1,2 +1,14 @@
 import z from 'zod';
 
+const schema = z.object({
+  name: z.string().min(2),
+  email: z.string().email(),
+  age: z.number().min(18).max(120)
+});
+
+let data = {
+  name: "Diones",
+  email: "diones@gmail.com.br",
+  age: 60
+}
+
